06a0e74534197024d96ebac58d58b8ee099c3783
nie23wp8738 <149007914+nie23wp8738@users.noreply.github.com>
2024-10-09 18:36:56 +0800

    Update HDNRA.cpp

--- src/HDNRA.cpp
+++ src/HDNRA.cpp
@@ -1268,11 +1268,11 @@ arma::vec zzz2022_glht_2cnrt_cpp(const Rcpp::List& Y, const arma::mat& X, const 
 
   // Calculate XtXinv using Cholesky decomposition
   arma::mat XtX = X.t() * X;
-  arma::mat XtXinv = cholesky_inverse(XtX); // Regularization removed
+  arma::mat XtXinv = cholesky_inverse(XtX + arma::eye(X.n_cols, X.n_cols) * 1e-10); // Regularization removed
 
   // Precompute XtXinv * C.t() and C * XtXinv * C.t() * inv
   arma::mat XtXinvC = XtXinv * C.t();
-  arma::mat invC_XtXinvC = cholesky_inverse(C * XtXinvC); // Regularization removed
+  arma::mat invC_XtXinvC = cholesky_inverse(C * XtXinvC + arma::eye(q, q) * 1e-10); // Regularization removed
 
   // Compute H matrix
   arma::mat H = X * XtXinvC * invC_XtXinvC * XtXinvC.t() * X.t();
