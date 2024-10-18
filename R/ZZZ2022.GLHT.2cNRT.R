f9d23b6c88d657b0fdf50e2b47e71abaf64b896b
nie23wp8738 <149007914+nie23wp8738@users.noreply.github.com>
2024-10-18 17:26:55 +0800

    Update ZZZ2022.GLHT.2cNRT.R

--- R/ZZZ2022.GLHT.2cNRT.R
+++ R/ZZZ2022.GLHT.2cNRT.R
@@ -1,5 +1,5 @@
 #' @title
-#' Normal-reference-test with two-cumulant (2-c) matched χ^2-approximation for GLHT problem proposed by Zhu et al. (2022)
+#' Normal-reference-test with two-cumulant (2-c) matched $\\chi^2$-approximation for GLHT problem proposed by Zhu et al. (2022)
 #' @description
 #' Zhu et al. (2022)'s test for general linear hypothesis testing (GLHT) problem for high-dimensional data with assuming that underlying covariance matrices are the same.
 #'
